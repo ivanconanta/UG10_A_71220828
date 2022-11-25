cb80bd126c8a50c2e45053d7b82f12902b7b553e
ivanconanta <118499597+ivanconanta@users.noreply.github.com>
2022-11-25 10:14:50 +0700

    Add files via upload

---
+++ 2_A_71220828.py
@@ -0,0 +1,44 @@
+#2.A. Test case 1
+print("===== Selamat datang di Toko Andi Tersenyum, selamat belanja! =====")
+a = int(input("Total belanja :Rp."))
+b = a-a*2/100
+c = a-a*5/100
+d = a-a*10/100
+if a>=100000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",b)
+elif a>=500000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",c)
+elif a>=1000000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",d)
+else:
+    print("Tidak ada diskon! Maka total yang anda bayarkan adalah",a)
+
+#2.B. Test case 2
+print("===== Selamat datang di Toko Andi Tersenyum, selamat belanja! =====")
+a = int(input("Total belanja :Rp."))
+b = a-a*2/100
+c = a-a*5/100
+d = a-a*10/100
+if a>=100000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",b)
+elif a>=500000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",c)
+elif a>=1000000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",d)
+else:
+    print("Tidak ada diskon! Maka total yang anda bayarkan adalah",a)
+
+#2.C. Test case 3
+print("===== Selamat datang di Toko Andi Tersenyum, selamat belanja! =====")
+a = int(input("Total belanja :Rp."))
+b = a-a*2/100
+c = a-a*5/100
+d = a-a*10/100
+if a>=100000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",b)
+elif a>=500000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",c)
+elif a>=1000000:
+    print("Biaya yang harus dibayar setelah diskon adalah: Rp.",d)
+else:
+    print("Tidak ada diskon! Maka total yang anda bayarkan adalah",a)
